5d6cc9d9d5738704841e0d1711c77a3a25809348
ajay26188 <ajaysah52@gmail.com>
2025-07-20 19:10:26 +0530

    get(:/id) fixed

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-require('express-async-errors')
 const app = express()
 
 const { PORT } = require('./util/config')
--- util/middleware.js
+++ util/middleware.js
@@ -1,3 +1,5 @@
+const { Blog } = require('../models')
+
 const blogFinder = async (req, res, next) => {
     const blog = await Blog.findByPk(req.params.id)
     if (!blog) {
